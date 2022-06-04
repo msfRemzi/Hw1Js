d42783fb57eec84318a6d04eff4f1e111e85443a
remzi <remzi.gthub@gmail.com>
2022-06-04 15:16:29 +0300

    first commit

---
+++ remzi.js
@@ -0,0 +1,22 @@
+
+let names = prompt('Введите имя пользователя');
+let ages  = +prompt('Введите возраст пользователя');
+
+let user = {
+    name: names,
+    age: ages,  
+}
+
+    for (let i = 1; i < 10; i++) {
+        let names = prompt('Введите имя пользователя');
+        let ages  = +prompt('Введите возраст пользователя');
+        
+        let user = {
+        name: names,
+        age: ages,  
+            }
+
+        console.log(user);
+    }
+
+console.log(user);
